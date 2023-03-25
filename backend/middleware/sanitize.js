faafb5ca9aa01881b9cb49479921cbb22b5b2902
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-03-25 10:33:24 +0100

    gitignore

--- backend/middleware/sanitize.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(require('sanitize').middleware);
-app.get('/ping', function(req, res) {
-	var param = req.queryInt('param');
-	res.send('pong ' + (typeof param) + ' ' + param);
-});
-app.listen(3000);
